6ac2398f8d14d2e2005fe97a97eb707b3fcb2b2b
Arkadiusz Ostrowski <arek.ostrowski135@gmail.com>
2026-01-17 17:06:37 +0100

    feat(api): define SignalR order event names

---
+++ src/OrderBoard.Api/Realtime/OrderEvents.cs
@@ -0,0 +1,8 @@
+namespace OrderBoard.Api.Realtime;
+
+public static class OrderEvents
+{
+    public const string OrderCreated = "OrderCreated";
+    public const string OrderUpdated = "OrderUpdated";
+    public const string OrderCanceled = "OrderCanceled";
+}
